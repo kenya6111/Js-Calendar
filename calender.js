a923d6e219de1488b7f568dbd4240a842f3fc07c
kenya6111 <kenyanke6111@gmail.com>
2025-06-03 21:00:51 +0900

    余分なソース削除

--- calender.js
+++ calender.js
@@ -1,19 +1,8 @@
-// console.log("hello world")
-
-// for(var i = 0;i < process.argv.length; i++){
-//     console.log("argv[" + i + "] = " + process.argv[i]);
-// }
-// console.log(process.argv.length)
-
-
-
 if(process.argv.length <= 2)
     {//引数指定のない場合
         const now = new Date();
         const currentYear = now.getFullYear()
         const currentMonth = now.getMonth()
-        const currentDay = now.getDay()
-        const currentDate = now.getDate()
 
         // 該当月の最終日取得
         now.setMonth(now.getMonth()+1, 0);
@@ -21,7 +10,7 @@ if(process.argv.length <= 2)
 
         // 該当月の初日取得
         now.setDate(1);
-        const firstDayOfCurrentMonth = now.getDay()
+        const firstDay = now.getDay()
 
         const arrday = ["日","月","火","水","木","金","土"]
         const arr1 = []
@@ -35,12 +24,12 @@ if(process.argv.length <= 2)
         for(let i=0;i<42;i++){
             // 第1週目を作成
             // --月の初日の曜日までは空白で埋める
-            if(i < firstDayOfCurrentMonth)
+            if(i < firstDay)
             {
                 arr1.push("  ")
                 continue
             }
-            // --7日目まで埋める
+            // --第１週目を作成
             if(i<7)
             {
                 arr1.push(dateCount.toString().padEnd(2," "))
@@ -84,12 +73,6 @@ if(process.argv.length <= 2)
                 continue
             }
         }
-        // console.log(now)
-        // console.log(currentYear)
-        // console.log(currentMonth)
-        // console.log(currentDate)
-        // console.log(endDay)
-        // console.log(currentDay)
 
         console.log(`     ${currentMonth+1}月 ${currentYear}`)
         console.log(arrday.join(" "))
@@ -101,28 +84,23 @@ if(process.argv.length <= 2)
         console.log(arr6.join(" "))
 
 
-    }
+}
 else if(process.argv[2] == "-m"
     && process.argv[3] >=0
     && process.argv[3] <=12
 ){
     const now = new Date();
         const currentYear = now.getFullYear()
-        const currentMonth = now.getMonth()
-        const currentDay = now.getDay()
-        const currentDate = now.getDate()
-
         const inputMonth = process.argv[3]
 
         // 該当月の最終日取得
-        // now.setMonth(now.getMonth()+1, 0);
         now.setMonth(inputMonth, 0);
         const endDate= now.getDate()
 
         // 該当月の初日取得
         now.setDate(1);
-        const firstDayOfCurrentMonth = now.getDay()
-        
+        const firstDay = now.getDay()
+
         const arrday = ["日","月","火","水","木","金","土"]
         const arr1 = []
         const arr2 = []
@@ -134,13 +112,13 @@ else if(process.argv[2] == "-m"
         let dateCount = 1
         for(let i=0;i<42;i++){
             // 第1週目を作成
-            // --月の初日の曜日までは空白で埋める
-            if(i < firstDayOfCurrentMonth)
+            // --月の初めの曜日までは空白で埋める
+            if(i < firstDay)
             {
                 arr1.push("  ")
                 continue
             }
-            // --7日目まで埋める
+            // 第1週目を作成
             if(i<7)
             {
                 arr1.push(dateCount.toString().padEnd(2," "))
@@ -171,25 +149,19 @@ else if(process.argv[2] == "-m"
             // 第5週目を作成
             if(i<35)
             {
-                if( dateCount > endDate)break;
+                if( dateCount > endDate ) break;
                 arr5.push(dateCount.toString().padEnd(2," "))
                 dateCount++
                 continue
             }
             if(i<42)
             {
-                if( dateCount > endDate)break;
+                if( dateCount > endDate) break;
                 arr6.push(dateCount.toString().padEnd(2," "))
                 dateCount++
                 continue
             }
         }
-        // console.log(now)
-        // console.log(currentYear)
-        // console.log(currentMonth)
-        // console.log(currentDate)
-        // console.log(endDay)
-        // console.log(currentDay)
 
         console.log(`     ${inputMonth}月 ${currentYear}`)
         console.log(arrday.join(" "))
@@ -200,4 +172,6 @@ else if(process.argv[2] == "-m"
         console.log(arr5.join(" "))
         console.log(arr6.join(" "))
 
+}else{
+    console.log("引数の値が不正です")
 }
