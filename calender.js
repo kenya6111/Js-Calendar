75e889a184a7626227b030b2c53a95d045d6f06d
kenya6111 <kenyanke6111@gmail.com>
2025-06-08 11:39:21 +0900

    リファクタ

--- calender.js
+++ calender.js
@@ -13,77 +13,26 @@ if(process.argv.length <= 2)
         const firstDay = now.getDay()
 
         const arrday = ["日","月","火","水","木","金","土"]
-        const arr1 = []
-        const arr2 = []
-        const arr3 = []
-        const arr4 = []
-        const arr5 = []
-        const arr6 = []
-
-        let dateCount = 1
-        for(let i=0;i<42;i++){
-            // 第1週目を作成
-            // --月の初日の曜日までは空白で埋める
-            if(i < firstDay)
-            {
-                arr1.push("  ")
-                continue
-            }
-            // --第１週目を作成
-            if(i<7)
-            {
-                arr1.push(dateCount.toString().padEnd(2," "))
-                dateCount++
-                continue
-            }
-            // 第2週目を作成
-            if(i<14)
-            {
-                arr2.push(dateCount.toString().padEnd(2," "))
-                dateCount++
-                continue
-            }
-            // 第3週目を作成
-            if(i<21)
-            {
-                arr3.push(dateCount.toString().padEnd(2," "))
-                dateCount++
-                continue
-            }
-            // 第4週目を作成
-            if(i<28)
-            {
-                arr4.push(dateCount.toString().padEnd(2," "))
-                dateCount++
-                continue
-            }
-            // 第5週目を作成
-            if(i<35)
-            {
-                if( dateCount > endDate)break;
-                arr5.push(dateCount.toString().padEnd(2," "))
-                dateCount++
-                continue
-            }
-            if(i<42)
-            {
-                if( dateCount > endDate)break;
-                arr6.push(dateCount.toString().padEnd(2," "))
-                dateCount++
-                continue
-            }
-        }
 
         console.log(`     ${currentMonth+1}月 ${currentYear}`)
         console.log(arrday.join(" "))
-        console.log(arr1.join(" "))
-        console.log(arr2.join(" "))
-        console.log(arr3.join(" "))
-        console.log(arr4.join(" "))
-        console.log(arr5.join(" "))
-        console.log(arr6.join(" "))
 
-
+        let dateCount = 1
+        for(let i=0;i<42;i++){
+            // 月曜が来たら改行
+            if (i!== 0 && i %7 ==0 ){
+                console.log()
+            }
+            if(i < firstDay){
+                // --月の初日の曜日までは空白で埋める
+                process.stdout.write("   ")
+                continue
+            } else {
+                if( dateCount > endDate)break;
+                process.stdout.write(dateCount.toString().padEnd(3," "))
+                dateCount++
+            }
+        }
 }
 else if(process.argv[2] == "-m"
     && process.argv[3] >=0
